f75db85d45719e9758ae9192407b32b532caf6f6
Jafer Daltro <jaferjr@gmail.com>
2020-10-26 21:24:59 -0300

    User validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ApplicationRecord  
+    EMAIL_VALIDATION = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates : username, presence :true,
+                uniqueness: { case_sensitive: false},
+                length: {minimum:3, maximum:25}
     
+    validates : email, presence :true,
+                uniqueness: { case_sensitive: false},
+                length: { maximum:105}
+                format: { with: EMAIL_VALIDATION}
+    
+
+
 end
