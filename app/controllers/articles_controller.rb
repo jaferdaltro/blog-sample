207c3c2183e3bdd59e053df5661381d7b369a17c
Jafer Daltro <jaferjr@gmail.com>
2020-10-29 19:25:04 -0300

    add one-to-many association between users and articles form back end

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,6 +21,7 @@ class ArticlesController < ApplicationController
 
     def create
         @article = Article.new(get_params)
+        @article.user = User.first
         if @article.save
             flash[:notice] = "Article was created successfully."
             redirect_to @article
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ApplicationRecord  
+    has_many :articles
+
     EMAIL_VALIDATION = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates : username, presence :true,
+    validates :username, presence: true,
                 uniqueness: { case_sensitive: false},
                 length: {minimum:3, maximum:25}
     
-    validates : email, presence :true,
+    validates :email, presence: true,
                 uniqueness: { case_sensitive: false},
-                length: { maximum:105}
+                length: { maximum: 105},
                 format: { with: EMAIL_VALIDATION}
-    
-
 
 end
