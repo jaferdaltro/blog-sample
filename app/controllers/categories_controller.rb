2fc6d1b3214fca4ecb1df7f946df03bd2a619346
Jafer Daltro <jaferjr@gmail.com>
2020-11-07 17:12:48 -0300

    created category, view, controller, model and association with articles

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,6 @@
 class CategoriesController < ApplicationController
+    before_action :require_admin, except: [:index, :show]
+
     def new
         @category = Category.new
     end
@@ -14,16 +16,41 @@ class CategoriesController < ApplicationController
     end
 
     def index
-        
+        @categories = Category.paginate(page: params[:page], per_page: 5)
     end
 
     def show
+        @category = Category.find(params[:id])
+        @articles = @category.articles.paginate(page: params[:page], per_page: 5)
     end
+
+    def edit
+        @category = Category.find(params[:id])
+    end
+
+    def update
+        @category = Category.find(params[:id])
+        if @category.update(category_params)
+            flash[:notice]= "This category was successfuly updated!"
+            redirect_to @category
+        else
+            render 'edit'
+        end
+    end
+    
+    
 
     private
 
     def category_params
         params.require(:category).permit(:name)
+    end
+
+    def require_admin
+        if !(logged_in? && current_user.admin?)
+            flash[:alert]="Only admins can perform that action"
+            redirect_to categories_path
+        end
     end
     
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_03_164559) do
+ActiveRecord::Schema.define(version: 2020_11_07_141403) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +23,11 @@ ActiveRecord::Schema.define(version: 2020_11_03_164559) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer "user_id"
+  end
+
+  create_table "articles_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
   end
 
   create_table "categories", force: :cascade do |t|
